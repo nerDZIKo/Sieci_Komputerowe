57b53171612b1775fc0f22d966518b2231734ecc
Jakub <jjakub.romanowski@gmail.com>
2025-02-16 02:42:07 +0100

    Poprawa Kodu. Dodanie TCP, Rutera i Switcha

--- Python_Skrypty/Warstwa Dostępy_Access Layer/Tworzenie_Ramki.py
+++ Python_Skrypty/Warstwa Dostępy_Access Layer/Tworzenie_Ramki.py
@@ -1,31 +1,75 @@
 import time
 import sys
 
-def animate_frame_transfer():
+def animate_frame_creation():
     frames = [
-        "[  ]",  # Pusta ramka
-        "[D ]",  # Dodanie adresu docelowego
-        "[DS]",  # Dodanie adresu źródłowego
-        "[DST]",  # Dodanie typu i długości
-        "[DSTP]",  # Dodanie danych
-        "[DSTPF]",  # Dodanie sumy kontrolnej
+        "[                      ]",  # Pusta ramka
+        "[MAC-DST: 3C:97:0E:67:89:AB]",  # Adres docelowy
+        "[MAC-DST: 3C:97:0E:67:89:AB | MAC-SRC: 00:1A:2B:3C:4D:5E]",  # Adres źródłowy
+        "[MAC-DST: 3C:97:0E:67:89:AB | MAC-SRC: 00:1A:2B:3C:4D:5E | TYPE: 0x0800]",  # Typ ramki (IPv4)
+        "[MAC-DST: 3C:97:0E:67:89:AB | MAC-SRC: 00:1A:2B:3C:4D:5E | TYPE: 0x0800 | IP-SRC: 192.168.1.10 | IP-DST: 10.0.0.5]",  # Adresy IP
+        "[MAC-DST: 3C:97:0E:67:89:AB | MAC-SRC: 00:1A:2B:3C:4D:5E | TYPE: 0x0800 | IP-SRC: 192.168.1.10 | IP-DST: 10.0.0.5 | TCP-SRC: 443 | TCP-DST: 56789]",  # Porty TCP
+        "[MAC-DST: 3C:97:0E:67:89:AB | MAC-SRC: 00:1A:2B:3C:4D:5E | TYPE: 0x0800 | IP-SRC: 192.168.1.10 | IP-DST: 10.0.0.5 | TCP-SRC: 443 | TCP-DST: 56789 | SEQ: 1001 | ACK: 2001 | FLAGS: SYN,ACK]",  # TCP handshake
+        "[MAC-DST: 3C:97:0E:67:89:AB | MAC-SRC: 00:1A:2B:3C:4D:5E | TYPE: 0x0800 | IP-SRC: 192.168.1.10 | IP-DST: 10.0.0.5 | TCP-SRC: 443 | TCP-DST: 56789 | SEQ: 1002 | ACK: 2002 | FLAGS: PSH,ACK | DATA: HTTP GET]",  # Dane
+        "[MAC-DST: 3C:97:0E:67:89:AB | MAC-SRC: 00:1A:2B:3C:4D:5E | TYPE: 0x0800 | IP-SRC: 192.168.1.10 | IP-DST: 10.0.0.5 | TCP-SRC: 443 | TCP-DST: 56789 | SEQ: 1002 | ACK: 2002 | FLAGS: PSH,ACK | DATA: HTTP GET | FCS: 0xA1B2C3D4]",  # Suma kontrolna
     ]
     
-    print("Tworzenie ramki Ethernet:")
+    print("Tworzenie pakietu Ethernet z TCP:")
     for frame in frames:
         sys.stdout.write("\r" + frame)
         sys.stdout.flush()
         time.sleep(0.5)
     
-    print("\nRamka została wysłana!\n")
+    print("\nPakiet został wysłany!\n")
+
+def animate_network_transfer():
+    frames = [
+        "[Host: Wysyłanie pakietu]",
+        "[Switch: Odbiór ramki | Odczyt MAC | Przekazanie do routera]",
+        "[Router: Odbiór pakietu | Odczyt IP | Routing]",
+        "[Router: Przekazanie pakietu do sieci docelowej]",
+        "[Switch: Odbiór ramki | Przekazanie do hosta docelowego]",
+        "[Host docelowy: Odbiór pakietu | Dekapsulacja | Przetwarzanie danych]",
+    ]
     
-    print("Dekapsulacja na odbiorniku:")
-    for frame in reversed(frames):
-        sys.stdout.write("\r" + frame)
+    details = [
+        "[MAC-DST: 3C:97:0E:67:89:AB | MAC-SRC: 00:1A:2B:3C:4D:5E]",
+        "[IP-SRC: 192.168.1.10 | IP-DST: 10.0.0.5]",
+        "[Router: Odczyt IP 10.0.0.5 | Wyszukiwanie trasy]",
+        "[Router: Przekazanie pakietu przez interfejs WAN | Nowy MAC-DST: 58:BF:EA:12:34:56 | MAC-SRC: 3C:97:0E:67:89:AB]",
+        "[Switch: Odbiór ramki | MAC-DST: 58:BF:EA:12:34:56 | MAC-SRC: 3C:97:0E:67:89:AB | Przekazanie do hosta]",
+        "[Host docelowy: Dekapsulacja | TCP SEQ: 1002 | ACK: 2002 | HTTP GET | Przetwarzanie danych]",
+    ]
+    
+    print("Symulacja komunikacji między hostem, switchem i routerem:")
+    for i in range(len(frames)):
+        sys.stdout.write("\r" + frames[i])
         sys.stdout.flush()
-        time.sleep(0.5)
+        time.sleep(1)
+        print("\n" + details[i])
     
-    print("\nDane odebrane i przetworzone!")
+    print("\nDane dotarły do hosta docelowego i zostały przetworzone!")
+
+def animate_decapsulation():
+    steps = [
+        "[Host docelowy: Otrzymano ramkę Ethernet]",
+        "[Usuwanie nagłówka Ethernet | Sprawdzanie MAC]",
+        "[Usuwanie nagłówka IP | Sprawdzanie adresu IP]",
+        "[Usuwanie nagłówka TCP | Sprawdzanie portów]",
+        "[Odczyt danych: HTTP GET]",
+        "[Dane gotowe do przetworzenia przez aplikację]",
+    ]
+    
+    print("\nProces dekapsulacji na hoście docelowym:")
+    for step in steps:
+        sys.stdout.write("\r" + step)
+        sys.stdout.flush()
+        time.sleep(1)
+        print("\n")
+    
+    print("\nKomunikat HTTP został przekazany do aplikacji!")
 
 if __name__ == "__main__":
-    animate_frame_transfer()
+    animate_frame_creation()
+    animate_network_transfer()
+    animate_decapsulation()
